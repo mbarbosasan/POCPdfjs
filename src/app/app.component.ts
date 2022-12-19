37f49c5932876f1654ce6f671c12ae526c814d45
mbarbosasan <moisesbarbosa23@gmail.com>
2022-12-18 21:27:58 -0300

    fim da POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'my-app';
+  file: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  uploadFile(file: any) {
+    const element = event?.currentTarget as HTMLInputElement;
+    let fileList = element!.files;
+
+    if (typeof (FileReader) !== "undefined") {
+      let reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.file = e.target.result;
+      }
+
+      reader.readAsArrayBuffer(fileList![0])
+
+    }
+
+
+
+    this.file = fileList;
+    console.log(file)
+    console.log(fileList);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
